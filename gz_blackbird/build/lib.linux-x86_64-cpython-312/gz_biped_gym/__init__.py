ce40d13fae83ab2d2bfbe56015a0aa8571dc2dc0
Ammar Hoque <aihoque2@illinois.edu>
2024-11-08 23:46:15 -0500

    first commit

--- gz_blackbird/build/lib.linux-x86_64-cpython-312/gz_biped_gym/__init__.py
+++
@@ -1,7 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='BlackBirdGazebo-v0',
-    entry_point='blackbird_gz:BlackbirdGazebo',
-    max_episode_steps=1000000,
-)
--- gz_blackbird/build/temp.linux-x86_64-cpython-312/CMakeFiles/train_simulator.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libtrain_simulator.a"
-)
