7602bd23e0937933799cd044f6811cbf58ef63a5
Ammar Hoque <aihoque2@illinois.edu>
2026-03-05 01:51:30 -0500

    update submodules

--- sync_repos.sh
+++ sync_repos.sh
@@ -10,11 +10,15 @@ git submodule sync --recursive
 echo "[3/6] Init/update submodules..."
 git submodule update --init --recursive
 
+echo "[4/6] Pull latest commits in each submodule..."
+git submodule foreach --recursive git pull origin HEAD
+
+echo "[5/6] Submodule status:"
+git submodule status
+
 if [ "${1:-}" = "--remote" ]; then
-  echo "[4/6] Updating submodules to latest remote branch..."
+  echo "[6/6] Updating submodules to latest remote branch..."
   git submodule update --remote --merge --recursive
-  echo "[5/6] Submodule status:"
-  git status --porcelain
 fi
 
 echo "Done."
